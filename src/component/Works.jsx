b24ad2ae8760b7a7485035c23d9685fdeeaabe2a
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-23 10:53:35 +0100

    feat Carousel with Embla Carousel

--- src/component/Works.jsx
+++ src/component/Works.jsx
@@ -1,27 +1,93 @@
-import React from 'react';
-import WorksSlider from './WorksSlider';
+import React, { useEffect, useRef } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import styled from "styled-components";
+import Image1 from '../img/1.jpg';
+import Image2 from '../img/2.jpg';
+import Image3 from '../img/3.jpg';
 
-const works = [
-  {
-    title: 'Titre de la diapositive 1',
-    imageUrl: './img/1.jpg',
-    linkUrl: 'lien_vers_votre_page_1',
-  },
-  {
-    title: 'Titre de la diapositive 2',
-    imageUrl: './img/2.jpg',
-    linkUrl: 'lien_vers_votre_page_2',
-  },
-  // Ajoutez d'autres diapositives selon vos besoins
-];
+const EmblaMain = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 0;
+  overflow: hidden;
+`;
 
-const App = () => {
+const Embla = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 80vw;
+  max-width: 100%;
+  height: 80vh;
+  margin: 0 auto;
+  padding: 0;
+`;
+
+const EmblaContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const EmblaSlide = styled.div`
+  flex: 0 0 100%;
+  min-width: 0;
+  align-items: center;
+`;
+
+const EmblaSlideImage = styled.img`
+  width: 80vw;
+  height: 100vh;
+  object-fit: cover;
+  max-width: 80vw;
+  max-height: 100vh;
+`;
+
+export function EmblaCarousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const scrolling = useRef(false);
+
+  useEffect(() => {
+    const handleWheel = (e) => {
+      if (!scrolling.current) {
+        scrolling.current = true;
+        setTimeout(() => {
+          scrolling.current = false;
+        }, 1500); // Limite du défilement à une action toutes les 500ms
+        if (emblaApi) {
+          if (e.deltaY > 0) {
+            emblaApi.scrollNext();
+          } else {
+            emblaApi.scrollPrev();
+          }
+        }
+      }
+    };
+
+    window.addEventListener('wheel', handleWheel);
+    return () => window.removeEventListener('wheel', handleWheel);
+  }, [emblaApi]);
+
   return (
-    <div>
-      <h1>Mon Slider</h1>
-	  <WorksSlider works={works} />
-    </div>
+      <EmblaMain>
+        <Embla ref={emblaRef}>
+          <EmblaContainer>
+            <EmblaSlide className="embla__slide">
+              <EmblaSlideImage src={Image1} alt="Slide 1" />
+            </EmblaSlide>
+            <EmblaSlide className="embla__slide">
+              <EmblaSlideImage src={Image2} alt="Slide 2" />
+            </EmblaSlide>
+            <EmblaSlide className="embla__slide">
+              <EmblaSlideImage src={Image3} alt="Slide 3" />
+            </EmblaSlide>
+          </EmblaContainer>
+        </Embla>
+      </EmblaMain>
   );
-};
+}
 
-export default App;
+export default EmblaCarousel;
