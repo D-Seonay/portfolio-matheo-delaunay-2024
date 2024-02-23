1c9f32121a25c28fc5dc080bf3c2969c10ea3dca
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-23 11:53:45 +0100

    style transition

--- src/component/Works.jsx
+++ src/component/Works.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import styled from "styled-components";
 import Image1 from '../img/1.jpg';
@@ -20,7 +20,7 @@ const EmblaMain = styled.div`
 const Embla = styled.div`
   overflow: hidden;
   position: relative;
-  width: 80vw;
+  width: 100vw;
   max-width: 100%;
   height: 80vh;
   margin: 0 auto;
@@ -29,25 +29,83 @@ const Embla = styled.div`
 
 const EmblaContainer = styled.div`
   display: flex;
-  align-items: center;
+  transition: transform 0.3s ease; /* Ajout d'une transition pour le déplacement des slides */
+  will-change: transform;
+  width: 100%;
+  height: 100%;
 `;
 
 const EmblaSlide = styled.div`
   flex: 0 0 100%;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
   min-width: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 10rem;
   align-items: center;
+  transition: opacity 0.5s ease; /* Ajout d'une transition pour l'opacité des slides */
 `;
 
+
+const EmblaContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+`;
+
+const EmblaSlideTitle = styled.h2`
+  font-size: 3rem;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+
+
 const EmblaSlideImage = styled.img`
-  width: 80vw;
-  height: 100vh;
+  position: absolute;
+  z-index: -1;
+  width: 60vw;
+  border-radius: 0.5rem;
+  height: 100%;
   object-fit: cover;
   max-width: 80vw;
-  max-height: 100vh;
+  max-height: 60vh;
+  margin: 0 auto;
+  filter: brightness(50%);
+`;
+
+const EmblaSlideLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.5rem;
+  transition: color 0.3s ease-in-out;
+  &:hover {
+    color: #ff0000;
+  }
+`;
+
+const ScrollText = styled.p`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  color: #000;
+  font-size: 0.5rem;
+  margin-bottom: 5rem;
+  opacity: 1;
+    transition: opacity 1s ease;
 `;
 
 export function EmblaCarousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
   const scrolling = useRef(false);
 
   useEffect(() => {
@@ -71,21 +129,71 @@ export function EmblaCarousel() {
     return () => window.removeEventListener('wheel', handleWheel);
   }, [emblaApi]);
 
+  const handleScroll = () => {
+    if (emblaApi) {
+      const currentScrollProgress = emblaApi.scrollProgress();
+      const shouldHideScrollText = currentScrollProgress >= 0.35; // Vous pouvez ajuster cette valeur selon vos besoins
+
+      // Masquer ou afficher le texte en fonction de la position de la slide
+      const scrollText = document.getElementById('scrollText');
+      if (scrollText) {
+        scrollText.style.opacity = shouldHideScrollText ? '0' : '1';
+      }
+    }
+  };
+
+  useEffect(() => {
+    emblaApi && emblaApi.on('scroll', handleScroll);
+    return () => emblaApi && emblaApi.off('scroll', handleScroll);
+  }, [emblaApi]);
+
   return (
       <EmblaMain>
         <Embla ref={emblaRef}>
           <EmblaContainer>
-            <EmblaSlide className="embla__slide">
-              <EmblaSlideImage src={Image1} alt="Slide 1" />
+            <EmblaSlide>
+              <EmblaContent>
+                <EmblaSlideImage src={Image1} alt="Slide 1" />
+                <EmblaSlideTitle>Slide 1</EmblaSlideTitle>
+                <EmblaSlideLink href="https://www.google.com" target="_blank">
+                  Lien vers Google
+                </EmblaSlideLink>
+              </EmblaContent>
             </EmblaSlide>
-            <EmblaSlide className="embla__slide">
-              <EmblaSlideImage src={Image2} alt="Slide 2" />
+
+            <EmblaSlide>
+                <EmblaContent>
+                    <EmblaSlideImage src={Image2} alt="Slide 2" />
+                    <EmblaSlideTitle>Slide 2</EmblaSlideTitle>
+                    <EmblaSlideLink href="https://www.google.com" target="_blank">
+                    Lien vers Google
+                    </EmblaSlideLink>
+                </EmblaContent>
             </EmblaSlide>
-            <EmblaSlide className="embla__slide">
-              <EmblaSlideImage src={Image3} alt="Slide 3" />
+
+            <EmblaSlide>
+                <EmblaContent>
+                    <EmblaSlideImage src={Image3} alt="Slide 3" />
+                    <EmblaSlideTitle>Slide 3</EmblaSlideTitle>
+                    <EmblaSlideLink href="https://www.google.com" target="_blank">
+                    Lien vers Google
+                    </EmblaSlideLink>
+                </EmblaContent>
+            </EmblaSlide>
+
+            <EmblaSlide>
+                <EmblaContent>
+                    <EmblaSlideImage src={Image1} alt="Slide 1" />
+                    <EmblaSlideTitle>Slide 4</EmblaSlideTitle>
+                    <EmblaSlideLink href="https://www.google.com" target="_blank">
+                    Lien vers Google
+                    </EmblaSlideLink>
+                </EmblaContent>
             </EmblaSlide>
           </EmblaContainer>
         </Embla>
+
+        <ScrollText id="scrollText">( SCROLL DOWN )</ScrollText>
       </EmblaMain>
   );
 }
