11fa4012bb642fd435dcd25e6a89c54b1a8c9371
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-29 09:53:09 +0100

    feat hero img

--- src/Images.jsx
+++ src/Images.jsx
@@ -2,5 +2,6 @@ import Phototheque from './img/projets/phototheque-de-chatons.webp';
 import CardFlipRevise from './img/projets/card-flip-revise.webp';
 import Pokedex from './img/projets/pokedex.webp';
 import GestionnaireDeBibliotheque from './img/projets/gestionnaire-de-bibliotheque.webp';
+import Hero from './img/MatheoDELAUNAY.png';
 
-export { Phototheque, CardFlipRevise, Pokedex, GestionnaireDeBibliotheque };
+export { Phototheque, CardFlipRevise, Pokedex, GestionnaireDeBibliotheque, Hero };
