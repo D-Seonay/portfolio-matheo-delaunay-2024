645dc3c0923069a4769338dde7e8a314e7583f56
Mathéo DELAUNAY <dseonay@gmail.com>
2024-07-31 13:56:49 +0200

    delete : loading animation

--- src/App.js
+++ src/App.js
@@ -9,29 +9,14 @@ import AboutMe from "./component/AboutMe";
 import Error404Page from "./component/Error404";
 import Footer from "./component/Footer";
 import ProjectDetailPage from './component/ProjectDetailPage';
-import Loading from './component/ui/Loading'; // Import du composant de chargement
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  // Simule le chargement initial
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Temps d'attente de 2 secondes pour l'exemple
-
-    return () => clearTimeout(timer); // Nettoie le timeout lorsque le composant est démonté
-  }, []);
-
   return (
     <>
       <GlassBackground>
         <Router>
           <Navbar />
           <CursorTracker />
-          {isLoading ? (
-            <Loading /> // Affiche le composant de chargement pendant le chargement initial
-          ) : (
             <Routes>
               <Route path="/" element={<Works />} />
               <Route path="/about" element={<AboutMe />} />
@@ -40,7 +25,6 @@ function App() {
               <Route path="/project/:id" element={<ProjectDetailPage />} />
               <Route path="*" element={<Error404Page />} />
             </Routes>
-          )}
           <Footer />
         </Router>
       </GlassBackground>
